f64d90b4904c57b1dd945389b277669ce0b99fd5
muhammed-mizaj <mdprgamer@gmail.com>
2023-05-08 22:11:46 +0530

    removed tailwind part

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-const BASE_URL = 'http://localhost:8000'
-
+const BASE_URL = "http://localhost:8000"
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -52,47 +51,31 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <header className="py-4 px-8 bg-white shadow-md">
-        <h1 className="text-2xl font-bold">List of TODOs</h1>
-      </header>
-      <main className="flex-1 container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="flex-1">
-            <ul className="border border-gray-300 p-4">
-              {todos.map((todo) => (
-                <li key={todo.id} className="mb-2">
-                  {todo.title}
-                </li>
-              ))}
-            </ul>
+    <div className="App" >
+      <div>
+        <h1>List of TODOs</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1>Create a ToDo</h1>
+        <form onSubmit={handleNewTodoSubmit}>
+          <div>
+            <label htmlFor="todo">ToDo: </label>
+            <input
+              type="text"
+              value={newTodo}
+              onChange={handleNewTodoChange}
+            />
           </div>
-          <div className="md:w-1/3">
-            <h2 className="text-xl font-bold mb-2">Create a ToDo</h2>
-            <form onSubmit={handleNewTodoSubmit}>
-              <div className="mb-2">
-                <label htmlFor="todo" className="font-bold">
-                  ToDo:{' '}
-                </label>
-                <input
-                  type="text"
-                  className="border border-gray-400 rounded px-2 py-1 w-full"
-                  value={newTodo}
-                  onChange={handleNewTodoChange}
-                />
-              </div>
-              <div className="mt-4">
-                <button
-                  type="submit"
-                  className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-                >
-                  Add ToDo!
-                </button>
-              </div>
-            </form>
+          <div style={{ marginTop: '5px' }}>
+            <button>Add ToDo!</button>
           </div>
-        </div>
-      </main>
+        </form>
+      </div>
     </div>
   );
 }
